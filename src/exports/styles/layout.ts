0dc30a7365ea6d6fe4e6c8fd1b1e0fc1c00a932a
githaka24 <githaka24@gmail.com>
2024-11-05 15:37:17 +0300

    style(layout): created and exported layout styles

---
+++ src/exports/styles/layout.ts
@@ -0,0 +1,4 @@
+export { default as HeaderStyles } from '@/styles/components/layout/header/Header.module.css';
+export { default as ContainerStyles } from '@/styles/components/layout/container/Container.module.css';
+export { default as MainStyles } from '@/styles/components/layout/main/Main.module.css';
+export { default as FooterStyles } from '@/styles/components/layout/footer/Footer.module.css';
