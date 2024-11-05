9874bc5b10959e1b5f89c5cc72118dbc998aa115
githaka24 <githaka24@gmail.com>
2024-11-05 13:15:58 +0300

    feat(exports): exported Container, Main & Footer components

--- src/exports/components/layout.ts
+++ src/exports/components/layout.ts
@@ -1,2 +1,5 @@
 export { default as Header } from '@/components/layout/header/Header';
 export { Brand, Navigation } from '@/components/layout/header/Header.fragments';
+export { default as Container } from '@/components/layout/container/Container';
+export { default as Main } from '@/components/layout/main/Main';
+export { default as Footer } from '@/components/layout/footer/Footer';
