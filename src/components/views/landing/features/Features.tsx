ce36c0a8210fc814a02f9ace30609b7e0af59b5f
githaka24 <githaka24@gmail.com>
2024-11-06 14:57:07 +0300

    style(views): updated Hero styles

---
+++ src/components/views/landing/features/Features.tsx
@@ -0,0 +1,12 @@
+import { View } from '@layout';
+
+export default function Features() {
+    return (
+        <View
+            id='featues'
+            style=''
+        >
+            <></>
+        </View>
+    );
+}
